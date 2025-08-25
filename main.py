1d9a18d1bc9f36acc517d401fa4bcb3f59b06137
AliALMaghamsi <almaghamsiali@gmail.com>
2025-08-25 13:44:23 +0300

    setting up the window for the astreoids game

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+def main():
+    pygame.init()
+    screen  = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    Running = True
+    while Running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,color=(0,0,0),rect=None)
+        pygame.display.flip()
+    
+if __name__ == "__main__":
+    main()
