dbf21bc63936cd6d19e9cf27e4f2b3d4c6fa9ff9
AliALMaghamsi <almaghamsiali@gmail.com>
2025-08-25 17:28:39 +0300

    made the player model

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
-    
+
     screen  = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    Running = True
+
     clock =pygame.time.Clock()
     dt=0
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2 , PLAYER_RADIUS)
+
+    Running = True
     while Running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,color=(0,0,0),rect=None)
-        pygame.display.flip()
+        player.draw(screen)
+        
         dt =clock.tick(60)
-        print(clock.get_fps())
+        
         dt = dt/1000
+        pygame.display.flip()
     
 if __name__ == "__main__":
     main()
