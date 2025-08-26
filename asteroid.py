b438e3fdd5fbe349e983fd9d95b0664f95e81159
AliALMaghamsi <almaghamsiali@gmail.com>
2025-08-26 11:07:57 +0300

    made the asteroids split if the bullet hit them

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 import pygame
-
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -14,3 +15,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle  = random.uniform(20,50)
+        first_astro_velocity = self.velocity.rotate(angle)
+        second_astro_velocity = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        astro1  = Asteroid(self.position.x,self.position.y,new_radius)
+        astro2  = Asteroid(self.position.x,self.position.y,new_radius)
+        astro1.velocity = first_astro_velocity * 1.2
+        astro2.velocity = second_astro_velocity* 1.2
+        
